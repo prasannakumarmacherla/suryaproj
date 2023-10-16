669b794f16b55d55527a55b0aedcda726139d623
prasannakumarmacherla <141391477+prasannakumarmacherla@users.noreply.github.com>
2023-10-16 15:36:00 +0530

    Add files via upload

---
+++ item.js
@@ -0,0 +1,15 @@
+document.getElementById("generate-password").addEventListener("click", function () {
+    var length = document.getElementById("password-length").value;
+    var password = generatePassword(length);
+    document.getElementById("password").value = password;
+});
+
+function generatePassword(length) {
+    var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*";
+    var password = "";
+    for (var i = 0; i < length; i++) {
+        var randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset.charAt(randomIndex);
+    }
+    return password;
+}
